cdb380f3ee90c9227b0e8af1f1fbbe7d531b1b0b
ashikulislamcse <ashikulislamcse@gmail.com>
2025-04-15 12:53:45 +0600

    Setup Complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
-
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Signup from "./Pages/Signup";
+import Signin from "./Pages/Signin";
+import Profile from "./Pages/Profile";
 const App = () => {
   return (
-    <div className='text-6xl'>
-      App
-    </div>
-  )
-}
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/about" element={<About/>}/>
+          <Route path="/sign-up" element={<Signup/>}/>
+          <Route path="/sign-in" element={<Signin/>}/>
+          <Route path="/profile" element={<Profile/>}/>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
 
-export default App
+export default App;
